5d4bec1e13699a2ec26551c6586084ef82c26d99
Franco Victorio <victorio.franco@gmail.com>
2017-11-28 19:19:53 -0300

    Initial commit

---
+++ src/main/scala/App.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+
+object App {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val sc = spark.sparkContext
+
+    val data = sc.parallelize(1 to 100)
+    val sum = data.reduce(_ + _)
+    println(s"Result: $sum")
+
+    spark.stop()
+  }
+}
